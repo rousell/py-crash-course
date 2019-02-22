7a5088ae683745670dc35ecd368f4823edb0a103
Lauren Rouse <lauren.l.rouse@gmail.com>
2019-02-22 09:53:23 -0600

    Started Ch 6, dictionaries

---
+++ ch_6/dictionaries.py
@@ -0,0 +1,12 @@
+## Person
+person = {'first_name': 'Thomas', 'last_name': 'Anderson', 'age': 20, 'city': 'Zion'}
+
+# print(person['first_name'])
+# print(person['last_name'])
+# print(person['age'])
+# print(person['city'])
+
+## Favorite Numbers
+fav_nums = {'lauren': 3, 'john': 42, 'carla': 6, 'steph': 12, 'george': 7}
+for key, value in fav_nums.items():
+    print(key + ': ' + str(value))
